9f4ac987e210dd319613352860842ead77858a22
Mat Pataki <matpataki@gmail.com>
2018-04-25 20:26:18 -0400

    first pass

--- setup_machine.sh
+++ setup_machine.sh
@@ -9,15 +9,7 @@ fi
 . git/setup_git.sh
 . vim/setup_vim.sh
 . tmux/setup_tmux.sh
-. NetworkManager/setup_NetworkManager.sh
 . gnupg/setup_gnupg.sh
-. x/setup_x.sh
-. intel_graphics/setup_intel_graphics.sh
-. awesome/setup_awesome.sh
-. xterm/setup_xterm.sh
-. xkb/setup_xkb.sh
-. xbacklight/setup_xbacklight.sh
-. synaptics/setup_synaptics.sh
 . java/setup_java.sh
 . docker/setup_docker.sh
 . slack/setup_slack.sh
--- tmux/setup_tmux.sh
+++ tmux/setup_tmux.sh
@@ -5,6 +5,8 @@
 function setup_tmux() {
   pacman_sync tmux
 
+  git_clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+
   check_and_link_file `pwd`/tmux/tmux.conf $HOME/.tmux.conf
 }
 
