ce3b5c9fdfa8fe2df840dbcb81db1a366d851162
Mat Pataki <matpataki@gmail.com>
2019-10-10 07:50:41 -0400

    install net-tools (mainly for netstat)

--- misc_tools/setup_misc_tools.sh
+++ misc_tools/setup_misc_tools.sh
@@ -16,6 +16,7 @@ function setup_misc_tools() {
   yaourt_sync networkmanager-openvpn
   yaourt_sync network-manager-applet
   yaourt_sync ulauncher
+  yaourt_sync net-tools
 
   gpg --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
   yaourt_sync dropbox
