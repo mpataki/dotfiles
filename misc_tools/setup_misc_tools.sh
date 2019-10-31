2a9fde9a64dc231d6ede8685e76b3b8e59285526
Mat Pataki <matpataki@gmail.com>
2019-10-31 09:52:57 -0400

    remove adobe

--- misc_tools/setup_misc_tools.sh
+++ misc_tools/setup_misc_tools.sh
@@ -19,7 +19,6 @@ function setup_misc_tools() {
   yaourt_sync net-tools
   yaourt_sync datagrip
   yaourt_sync packer
-  yaourt_sync acroread
 
   gpg --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
   yaourt_sync dropbox
