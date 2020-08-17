26e4b71e377f777c5d360681bbb1275b26502526
Mat Pataki <matpataki@gmail.com>
2020-08-17 16:19:34 -0400

    add 2.2.3

--- ruby/setup_ruby.sh
+++ ruby/setup_ruby.sh
@@ -3,6 +3,9 @@
 function setup_ruby() {
   yaourt_sync rbenv
   yaourt_sync ruby-build
+
+  # 2.2.3... hopefully not needed for much longer
+  curl -fsSL https://gist.github.com/mislav/055441129184a1512bb5.txt | PKG_CONFIG_PATH=/usr/lib/openssl-1.0/pkgconfig rbenv install --patch 2.2.3
 }
 
 print_with_color $YELLOW 'Setup Ruby? (yes/no)'
