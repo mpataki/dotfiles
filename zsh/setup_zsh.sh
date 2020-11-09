4565afaba0f4d2c533c4dab3ddfc83e7ea96c251
Mat Pataki <matpataki@gmail.com>
2020-11-08 23:43:06 -0500

    use powerline theme

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,3 +1,5 @@
+. lib/helpers.sh
+
 function write_zshrc() {
   print_with_color $GREEN "writing $HOME/.zshrc"
 
@@ -7,19 +9,30 @@ function write_zshrc() {
 function setup_zsh(){
   pacman_sync 'zsh'
   pacman_sync 'zsh-completions'
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-  # hub completions
-  mkdir -p ~/.zsh/completions
-  cp /usr/local/share/zsh/site-functions/_hub ~/.zsh/completions/_hub
+  # oh-my-zshell
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-  git_clone 'https://github.com/geometry-zsh/geometry.git' "$HOME/.oh-my-zsh/custom/themes/geometry"
+
+  # theme
+  git_clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+  wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+  wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+
+  mkdir -p ~/.local/share/fonts
+  mv PowerlineSymbols.otf ~/.local/share/fonts/
+
+  fc-cache -vf ~/.local/share/fonts/
+
+  mkdir -p ~/.config/fontconfig/conf.d/
+  mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 
   if [ -e $HOME/.zshrc ]; then
     print_with_color $YELLOW "$HOME/.zshrc already exists. Do you want to override it? (yes/no)"
     read yn
     case $yn in
-      yes ) write_zshrc;;
+      yes|Yes|YES|y|Y ) write_zshrc;;
       * ) print_with_color $GREEN 'skipping...';;
     esac
   else
@@ -29,7 +42,7 @@ function setup_zsh(){
   print_with_color $YELLOW 'Do you want to set zsh as the default shell? (yes/no)'
   read yn
   case $yn in
-    yes ) chsh -s /bin/zsh;;
+    yes|Yes|YES|y|Y ) chsh -s /bin/zsh;;
     * ) print_with_color $GREEN 'skipping...';;
   esac
 }
@@ -37,6 +50,6 @@ function setup_zsh(){
 print_with_color $YELLOW 'Setup zsh? (yes/no)'
 read yn
 case $yn in
-  yes ) setup_zsh;;
+  yes|Yes|YES|y|Y ) setup_zsh;;
   * ) print_with_color $GREEN 'skipping...';;
 esac
