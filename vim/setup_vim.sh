0bc5adafb9450828dea32a11a1ee63d97c1b7b7a
Mat Pataki <matpataki@gmail.com>
2020-11-09 23:28:39 -0500

    remove you complete me

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -1,20 +1,19 @@
+#!/bin/bash
+. lib/helpers.sh
+
 function setup_vim(){
   pacman_sync gvim
   pacman_sync the_silver_searcher
 
-  print_with_color $YELLOW 'this next one takes 10+ minutes to install..'
-  yaourt_sync vim-youcompleteme-git
-
   check_and_link_file `pwd`/vim/vimrc $HOME/.vimrc
   check_and_link_file `pwd`/vim/colors $HOME/.vim/colors
-  check_and_link_file `pwd`/vim/ycm_extra_conf.py $HOME/.vim/ycm_extra_conf.py
 
   if ! [ -e $HOME/.vim ]; then
     mkdir $HOME/.vim
   fi
 }
 
-print_with_color $YELLOW 'Setup Vim? (yes/no)'
+print_with_color $YELLOW 'Setup Vim? (y/n)'
 read yn
 case $yn in
   yes|Yes|YES|y|Y ) setup_vim;;
