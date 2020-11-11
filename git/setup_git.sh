7581cc8807c191997ca7496a391d75f04b9aff96
Mat Pataki <matpataki@gmail.com>
2020-11-10 22:26:28 -0500

    remove hub

--- git/setup_git.sh
+++ git/setup_git.sh
@@ -1,19 +1,16 @@
+#!/bin/bash
+. lib/helpers.sh
 
 function setup_git() {
   pacman_sync git
-  pacman_sync hub
   pacman_sync github-cli
   pacman_sync diff-so-fancy
 
   check_and_link_file `pwd`/git/gitconfig $HOME/.gitconfig
   check_and_link_file `pwd`/git/gitignore $HOME/.gitignore
-
-  # hub completions
-  mkdir -p ~/.zsh/completions
-  cp /usr/local/share/zsh/site-functions/_hub ~/.zsh/completions/_hub
 }
 
-print_with_color $YELLOW 'Setup Git? (yes/no)'
+print_with_color $YELLOW 'Setup Git? (y/n)'
 read yn
 case $yn in
   yes|Yes|YES|y|Y ) setup_git;;
