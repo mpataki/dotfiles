0ae4231415478b9ea087c7a7b1e97ec9a3260925
Mat Pataki <matpataki@gmail.com>
2022-01-31 20:49:58 -0500

    avoid simple terminal for now... it's too complicated

--- setup_machine.sh
+++ setup_machine.sh
@@ -22,7 +22,7 @@ fi
 #. keyboard/setup_keyboard.sh
 . xfce/setup_xfce.sh
 . tablet/setup_tablet.sh
-. simple_terminal/setup_simple_terminal.sh
+#. simple_terminal/setup_simple_terminal.sh
 #. virtual_keyboard/setup_virtual_keyboard.sh
 
 print_with_color $GREEN "Done."
