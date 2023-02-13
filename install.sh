66a1ad021b03832632d366174bd0a845aa81ce0b
Mat Pataki <matpataki@gmail.com>
2023-02-13 10:27:25 -0500

    rename setup_machine.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+. lib/helpers.sh
+
+if ! [[ `which sudo` ]]; then
+  echo "sudo needs to be installed for this to work"
+  exit 1
+fi
+
+# mac
+if is_mac; then
+  if ! [[ -e `which brew` ]]; then
+    print_with_color $YELLOW "Installing Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
+  print_with_color $GREEN 'Updating Homebrew'
+  brew update
+else # linux
+  . yay/setup_yay.sh
+  . snap/setup_snap.sh
+  . vim/setup_vim.sh # this should be on mac also, but could use some modernization first.
+  #. keyboard/setup_keyboard.sh
+  #. tablet/setup_tablet.sh
+fi
+
+# both
+. zsh/setup_zsh.sh
+. git/setup_git.sh
+. tmux/setup_tmux.sh
+. java/setup_java.sh
+. dotnet/setup_dotnet.sh
+. ruby/setup_ruby.sh
+. python/setup_python.sh
+. docker/setup_docker.sh
+. misc_tools/setup_misc_tools.sh
+
+print_with_color $GREEN "Done."
