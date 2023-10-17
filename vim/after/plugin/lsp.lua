891573f0733270de150b8fc8652edf83024352fe
Mat Pataki <matpataki@gmail.com>
2023-10-17 00:04:19 -0400

    lsp setup

--- vim/after/plugin/lsp.lua
+++ vim/after/plugin/lsp.lua
@@ -1,17 +1,41 @@
-local lsp_zero = require('lsp-zero')
+local lsp = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, bufnr)
+-- Fix Undefined global 'vim'
+lsp.configure('lua_ls', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
-	lsp_zero.default_keymaps(opts)
+	lsp.default_keymaps(opts)
 end)
 
-require('lspconfig').lua_ls.setup({})
-require('lspconfig').rust_analyzer.setup({})
-require('lspconfig').tsserver.setup({})
-require('lspconfig').eslint.setup({})
-require('lspconfig').java_language_server.setup({})
-require('lspconfig').bashls.setup({})
-require('lspconfig').yamlls.setup({})
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+	-- Replace the language servers listed here 
+	-- with the ones you want to install
+	ensure_installed = {
+		'lua_ls',
+		'rust_analyzer',
+		'tsserver',
+		'eslint',
+		-- 'java_language_server',
+		'bashls',
+		'yamlls',
+	},
+	handlers = {
+		lsp.default_setup,
+	},
+})
+
+lsp.setup()
