f5a9eae0839e4372ab104449ee941213e0a0279f
Mat Pataki <matpataki@gmail.com>
2023-11-01 18:51:49 -0400

    vim keymap - yank to mac clipboard

--- vim/lua/mpataki/remap.lua
+++ vim/lua/mpataki/remap.lua
@@ -2,9 +2,9 @@
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- copy to mac clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", '"*y')
+vim.keymap.set("n", "<leader>Y", '"*Y')
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
