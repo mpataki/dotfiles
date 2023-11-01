22b529e06e00767e6519c6708f5db322af4531cd
Mat Pataki <matpataki@gmail.com>
2023-11-01 19:11:39 -0400

    vim settings for netrw

--- vim/lua/mpataki/set.lua
+++ vim/lua/mpataki/set.lua
@@ -32,3 +32,9 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
+-- netrw (the file tree) settings
+vim.g.netrw_liststyle = 3
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 30
+vim.g.netrw_browse_split = 4
+
