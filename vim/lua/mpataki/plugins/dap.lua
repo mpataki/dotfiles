31afae2cd2ec388f42cb19deb2a0f7334282263e
Mat Pataki <matpataki@gmail.com>
2024-01-14 09:50:46 -0500

    remove extra dap mapping

--- vim/lua/mpataki/plugins/dap.lua
+++ vim/lua/mpataki/plugins/dap.lua
@@ -17,7 +17,6 @@ return {
             vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
             vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
             vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-            vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
             vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
 
             vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
