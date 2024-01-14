ad0dc0a77d27a8edd1d17b30260f328f6dccb2c5
Mat Pataki <matpataki@gmail.com>
2024-01-14 09:50:33 -0500

    refactor neo-tree

--- vim/lua/mpataki/plugins/init.lua
+++ vim/lua/mpataki/plugins/init.lua
@@ -30,17 +30,6 @@ return {
 
     { 'mfussenegger/nvim-jdtls' },
 
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons",
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
-    },
-
     -- colorschemes
     -- { 'srcery-colors/srcery-vim', lazy=true, priority=1000 },
     { 'kabbamine/yowish.vim', lazy=true, priority=1000 },
---
+++ vim/lua/mpataki/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    lazy = false,
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                hijack_netrw_behavior = "open_current"
+            }
+        })
+
+        vim.keymap.set('n', '<Leader>t', '<cmd>:Neotree position=current<CR>')
+    end
+}
