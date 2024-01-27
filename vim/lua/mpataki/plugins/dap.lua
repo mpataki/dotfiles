47f69b16acfcae80eff551bbafd82c7a7484979d
Mat Pataki <matpataki@gmail.com>
2024-01-27 13:35:34 -0500

    dap config

--- vim/lua/mpataki/plugins/dap.lua
+++ vim/lua/mpataki/plugins/dap.lua
@@ -43,9 +43,12 @@ return {
             end)
 
             vim.keymap.set({'n', 'v'}, '<Leader>du', function()
-                require('jdtls.dap').setup_dap_main_class_configs()
-                require('dap.ext.vscode').load_launchjs('./launch.json')
                 require("dapui").toggle()
+            end)
+
+            vim.keymap.set({'n', 'v'}, '<Leader>dl', function()
+                -- require('jdtls.dap').setup_dap_main_class_configs()
+                require('dap.ext.vscode').load_launchjs('./launch.json')
             end)
 
             require('telescope').load_extension('dap')
