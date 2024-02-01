c2cb6fba5f6ef8e23b7410df952510f7a7b07ee0
Mat Pataki <matpataki@gmail.com>
2024-01-31 22:12:58 -0500

    try out neotree in the traditional way

--- vim/lua/mpataki/plugins/neo-tree.lua
+++ vim/lua/mpataki/plugins/neo-tree.lua
@@ -14,6 +14,7 @@ return {
             }
         })
 
-        vim.keymap.set('n', '<Leader>t', '<cmd>:Neotree position=current<CR>')
+        vim.keymap.set('n', '<Leader>t', '<cmd>:Neotree<CR>')
+        -- vim.keymap.set('n', '<Leader>t', '<cmd>:Neotree position=current<CR>')
     end
 }
