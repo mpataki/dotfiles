cafc1000ab1e85f1333ae0bf38aca552b6ae0cc4
Mat Pataki <matpataki@gmail.com>
2024-03-26 21:23:34 -0400

    try another colorscheme.. I have a problem

--- vim/lua/mpataki/plugins/colors.lua
+++ vim/lua/mpataki/plugins/colors.lua
@@ -2,19 +2,28 @@
 -- colorschemes
 -- { 'srcery-colors/srcery-vim', lazy=true, priority=1000 },
 return {
-    'folke/tokyonight.nvim',
+    -- 'folke/tokyonight.nvim',
     -- 'kabbamine/yowish.vim',
+    'catppuccin/nvim',
     lazy = false,
     priority = 1000,
     config = function()
-        require("tokyonight").setup({
-            style = 'night',
-            transparent = true,
+        require("catppuccin").setup({
+            flavor = 'mocha',
+            transparent_background = true,
         })
 
-        vim.cmd.colorscheme("tokyonight")
+        vim.cmd.colorscheme("catppuccin")
 
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+
+        -- require("tokyonight").setup({
+        --     style = 'night',
+        --     transparent = true,
+        -- })
+        --
+        -- vim.cmd.colorscheme("tokyonight")
+
+        -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#141414" })
 
         -- disable the background in floating windows
