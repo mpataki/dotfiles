7a1c455c906b04e18a5d0f224aa180362e8412f8
Mat Pataki <matpataki@gmail.com>
2024-04-13 18:38:30 -0400

    add cmp-dap

--- vim/lua/mpataki/plugins/cmp.lua
+++ vim/lua/mpataki/plugins/cmp.lua
@@ -93,4 +93,21 @@ return {
             vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link = 'CmpItemKindKeyword' })
         end
     },
+    {
+        'rcarriga/cmp-dap',
+        config = function ()
+            require("cmp").setup({
+                enabled = function()
+                    return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
+                    or require("cmp_dap").is_dap_buffer()
+                end
+            })
+
+            require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+                sources = {
+                    { name = "dap" },
+                },
+            })
+        end
+    }
 }
