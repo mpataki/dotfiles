e79628b4c2ee1c83615d36f0941454530d3f104e
Mat Pataki <matpataki@gmail.com>
2024-04-13 18:37:44 -0400

    notify config

--- vim/lua/mpataki/plugins/notify.lua
+++ vim/lua/mpataki/plugins/notify.lua
@@ -4,5 +4,9 @@ return {
         vim.opt.termguicolors = true
 
         vim.notify = require('notify')
+
+        vim.notify.setup({
+            background_colour = "#000000",
+        })
     end
 }
