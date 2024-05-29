bd276e6a9bc770c0d0739bff840be0bfa5c61e1c
Mat Pataki <matpataki@gmail.com>
2024-05-28 22:51:00 -0400

    set some fun wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,12 +2,34 @@ local wezterm = require 'wezterm'
 
 local config = {}
 
-config.color_scheme = 'Brogrammer'
+config.color_scheme = 'Brewer (base16)'
+
 config.colors = {
     background = '#000000'
 }
+
 config.font = wezterm.font 'Hack Nerd Font Mono'
+
 config.hide_tab_bar_if_only_one_tab = true
 
-return config
+config.keys = {
+  {
+      key = 'O',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action_callback(function(window, pane)
+          local sel = window:get_selection_text_for_pane(pane)
+          wezterm.open_with(sel)
+      end)
+  },
+}
 
+-- config.mouse_bindings = {
+--   -- Ctrl-click will open the link under the mouse cursor
+--   {
+--     event = { Up = { streak = 1, button = 'Left' } },
+--     mods = 'CTRL',
+--     action = wezterm.action.OpenLinkAtMouseCursor,
+--   },
+-- }
+
+return config
