0e05cb0b7f714347b9f799ca15a1532ecea78614
Mat Pataki <matpataki@gmail.com>
2024-05-28 23:01:13 -0400

    cleanup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -30,13 +30,4 @@ config.keys = {
   },
 }
 
--- config.mouse_bindings = {
---   -- Ctrl-click will open the link under the mouse cursor
---   {
---     event = { Up = { streak = 1, button = 'Left' } },
---     mods = 'CTRL',
---     action = wezterm.action.OpenLinkAtMouseCursor,
---   },
--- }
-
 return config
