fe835c052fdaf604759d1cf0f24704c8ccd0ccb9
Mat Pataki <matpataki@gmail.com>
2024-05-28 22:51:16 -0400

    hack to get this browser env var out of here... yikes

--- shell/environment.sh
+++ shell/environment.sh
@@ -3,7 +3,6 @@
 export NOTES_S3_BUCKET=note-store
 export LOCAL_NOTE_STORE=$HOME/notes
 export EDITOR=/opt/homebrew/bin/nvim
-export BROWSER=/usr/bin/brave
 export GPG_TTY=$(tty)
 #export SPARK_HOME=/home/mat/code/spark-2.1.0-bin-hadoop2.7
 #export SPARK_HOME=/home/mat/code/spark-2.4.3-bin-hadoop2.7
@@ -12,6 +11,9 @@ export SPARK_HOME=/home/mat/code/spark-3.1.1-bin-hadoop3.2
 export PATH="/home/mat/code/kafka-3.2.0-src/bin/:$PATH"
 
 export GRAALVM_HOME=/usr/lib/jvm/java-17-graalvm
+
+# hack.. not sure what's setting BROWSER right now
+unset BROWSER
 
 # GH CLI
 export CLICOLOR_FORCE=true
