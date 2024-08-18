f6a16e3ff52187f23fd1538251db37fd5694b3ac
Mat Pataki <matpataki@gmail.com>
2024-08-18 15:19:15 -0400

    cut gpg tty for now, too slow

--- shell/environment.sh
+++ shell/environment.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-export NOTES_S3_BUCKET=note-store
-export LOCAL_NOTE_STORE=$HOME/notes
 export EDITOR=/opt/homebrew/bin/nvim
-export GPG_TTY=$(tty)
-#export SPARK_HOME=/home/mat/code/spark-2.1.0-bin-hadoop2.7
-#export SPARK_HOME=/home/mat/code/spark-2.4.3-bin-hadoop2.7
+#export GPG_TTY=$(tty)
 export SPARK_HOME=/home/mat/code/spark-3.1.1-bin-hadoop3.2
 
 export PATH="/home/mat/code/kafka-3.2.0-src/bin/:$PATH"
