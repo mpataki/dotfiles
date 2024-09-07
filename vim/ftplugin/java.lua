4af8edc70586f21447f4b09ff398955dbd5f299f
Mat Pataki <matpataki@gmail.com>
2024-09-07 07:56:35 -0400

    fix jdtls after system update

--- vim/ftplugin/java.lua
+++ vim/ftplugin/java.lua
@@ -6,13 +6,13 @@ local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(
 -- local inspect = require('vim.inspect')
 
 local cmd = {
-    vim.fn.glob("/opt/homebrew/Cellar/openjdk/21.*/bin/java", true),
+    vim.fn.glob("/opt/homebrew/Cellar/openjdk@21/21.*/bin/java", true),
 
     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
     '-Dosgi.bundles.defaultStartLevel=4',
     '-Declipse.product=org.eclipse.jdt.ls.core.product',
     '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
+    '-Dlog.level=WARN',
     '-Xmx1g',
     '--add-modules=ALL-SYSTEM',
     '--add-opens', 'java.base/java.util=ALL-UNNAMED',
