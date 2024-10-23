4f487a2174f50942a21608d06ead9521b10aa5da
Mat Pataki <matpataki@gmail.com>
2024-10-23 08:40:50 -0400

    obsidian nvim basic configuration

--- nvim/lua/mpataki/plugins/obsidian.lua
+++ nvim/lua/mpataki/plugins/obsidian.lua
@@ -2,16 +2,14 @@
 return {
   "epwalsh/obsidian.nvim",
   version = "*",  -- recommended, use latest release instead of latest commit
-  lazy = false,
-  -- ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  event = {
-    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    -- refer to `:h file-pattern` for more examples
-    "BufReadPre " .. vim.fn.expand("~") .. "/obsidian-notes-vault/*.md",
-    "BufNewFile " .. vim.fn.expand("~") .. "/obsidian-notes-vault/*.md",
-  },
+  -- Replace the below lines with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre " .. vim.fn.expand("~") .. "/obsidian-notes-vault/*.md",
+  --   "BufNewFile " .. vim.fn.expand("~") .. "/obsidian-notes-vault/*.md",
+  -- },
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
@@ -27,7 +25,7 @@ return {
     },
   },
   -- mappings = {},
-  config = function ()
-      vim.keymap.set('n', '<leader>os', '<cmd>:ObsidianSearch<CR>')
-  end
+--   config = function ()
+--       vim.keymap.set('n', '<leader>os', '<cmd>:ObsidianSearch<CR>')
+--   end
 }
