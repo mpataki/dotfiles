c687094c94547ee264a872e9913992593634bd08
Mat Pataki <matpataki@gmail.com>
2024-11-03 10:25:13 -0500

    some more obsidian config

--- nvim/lua/mpataki/plugins/lazydev.lua
+++ nvim/lua/mpataki/plugins/lazydev.lua
@@ -1,3 +1,4 @@
+-- https://github.com/folke/lazydev.nvim
 return {
   {
     "folke/lazydev.nvim",
--- nvim/lua/mpataki/plugins/obsidian.lua
+++ nvim/lua/mpataki/plugins/obsidian.lua
@@ -13,19 +13,29 @@ return {
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "personal",
-        path = vim.fn.expand("~") .. "/obsidian-notes-vault",
-      },
-    },
   },
   -- mappings = {},
---   config = function ()
---       vim.keymap.set('n', '<leader>os', '<cmd>:ObsidianSearch<CR>')
---   end
+  config = function ()
+      require('obsidian').setup({
+          workspaces = {
+              {
+                  name = "personal",
+                  path = vim.fn.expand("~") .. "/obsidian-notes-vault",
+              },
+          },
+          daily_notes = {
+              -- Optional, if you keep daily notes in a separate directory.
+              folder = "dailies",
+              -- Optional, if you want to change the date format for the ID of daily notes.
+              date_format = "%Y-%m-%d",
+              -- Optional, if you want to change the date format of the default alias of daily notes.
+              alias_format = "%B %-d, %Y",
+              -- Optional, default tags to add to each new daily note created.
+              default_tags = { "daily-notes" },
+              -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+              template = nil
+          },
+      })
+      vim.keymap.set('n', '<leader>os', ':ObsidianSearch<CR>', { noremap = true, silent = true })
+  end
 }
