8483b7b5dda5c38c99609f58fafdb527b79ea5aa
Mat Pataki <matpataki@gmail.com>
2024-11-03 22:31:58 -0500

    improve shell start up time

--- shell/environment.sh
+++ shell/environment.sh
@@ -2,11 +2,11 @@
 
 export EDITOR=/opt/homebrew/bin/nvim
 #export GPG_TTY=$(tty)
-export SPARK_HOME=/home/mat/code/spark-3.1.1-bin-hadoop3.2
+#export SPARK_HOME=/home/mat/code/spark-3.1.1-bin-hadoop3.2
 
-export PATH="/home/mat/code/kafka-3.2.0-src/bin/:$PATH"
+#export PATH="/home/mat/code/kafka-3.2.0-src/bin/:$PATH"
 
-export GRAALVM_HOME=/usr/lib/jvm/java-17-graalvm
+# export GRAALVM_HOME=/usr/lib/jvm/java-17-graalvm
 
 # hack.. not sure what's setting BROWSER right now
 unset BROWSER
