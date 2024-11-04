4280c37038cabde07551e82d07b738301c1998c4
Mat Pataki <matpataki@gmail.com>
2024-11-03 22:30:20 -0500

    vimux key maps

--- nvim/lua/mpataki/plugins/vimux.lua
+++ nvim/lua/mpataki/plugins/vimux.lua
@@ -4,11 +4,11 @@ return {
         vim.g.VimuxOrientation = "h"
         vim.g.VimuxHeight = "38%"
 
-        vim.keymap.set('n', '<leader>vp', ':VimuxPromptCommand<CR>', { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>vl', ':VimuxRunLastCommand<CR>', { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>vq', ':VimuxCloseRunner<CR>', { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>vx', ':VimuxInterruptRunner<CR>', { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>vz', ':VimuxZoomRunner<CR>', { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>vk', ':VimuxClearTerminalScreen<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>tp', ':VimuxPromptCommand<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>tl', ':VimuxRunLastCommand<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>tq', ':VimuxCloseRunner<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>tx', ':VimuxInterruptRunner<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>tz', ':VimuxZoomRunner<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>tk', ':VimuxClearTerminalScreen<CR>', { noremap = true, silent = true })
     end
 }
