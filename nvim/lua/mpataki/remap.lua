c1842dce2c37109ec1629c37953dc11d4498f5a3
Mat Pataki <matpataki@gmail.com>
2024-11-25 21:59:02 -0500

    update split resizing keymaps to account for aerospace collision

--- nvim/lua/mpataki/remap.lua
+++ nvim/lua/mpataki/remap.lua
@@ -19,10 +19,10 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
 -- split resizing
-vim.keymap.set("n", "<M-h>", "<c-w>5<")
-vim.keymap.set("n", "<M-l>", "<c-w>5>")
-vim.keymap.set("n", "<M-k>", "<C-w>+")
-vim.keymap.set("n", "<M-j>", "<C-w>-")
+vim.keymap.set("n", "<MC-h>", "<c-w>5<")
+vim.keymap.set("n", "<MC-l>", "<c-w>5>")
+vim.keymap.set("n", "<MC-k>", "<C-w>+")
+vim.keymap.set("n", "<MC-j>", "<C-w>-")
 
 -- next and previous buffers
 vim.keymap.set("n", '<leader>n', ":bnext<CR>")
@@ -33,13 +33,13 @@ vim.keymap.set("n", '<c-o>', "<c-o>zz");
 vim.keymap.set("n", '<c-i>', "<c-i>zz");
 
 -- quickfix
-vim.keymap.set("n", "<leader>qo", ":copen<CR>")
-vim.keymap.set("n", "<leader>qc", ":cclose<CR>")
-vim.keymap.set("n", "<leader>qq", ":cclose<CR>")
-vim.keymap.set("n", "<leader>qn", ":cnext<CR>")
-vim.keymap.set("n", "<leader>qp", ":cprev<CR>")
+vim.keymap.set("n", "<leader>co", ":copen<CR>")
+vim.keymap.set("n", "<leader>cc", ":cclose<CR>")
+vim.keymap.set("n", "<leader>cq", ":cclose<CR>")
+vim.keymap.set("n", "<leader>cn", ":cnext<CR>")
+vim.keymap.set("n", "<leader>cp", ":cprev<CR>")
 
 vim.keymap.set("n", "<leader>w", ":w<CR>", { nowait = true, silent = true })
-vim.keymap.set("n", "<leader>x", ":q<CR>", { nowait = true, silent = true })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { nowait = true, silent = true })
 vim.keymap.set("n", "<leader>sv", ":vsplit<CR><C-w>l");
 vim.keymap.set("n", "<leader>sh", ":split<CR><C-w>j");
