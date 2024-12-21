ec506b1f354377f581cecd0ebfe159a6f0885812
Mat Pataki <matpataki@gmail.com>
2024-12-21 11:02:15 -0500

    tsx grammer for treesitter

--- nvim/lua/mpataki/plugins/treesitter.lua
+++ nvim/lua/mpataki/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
             ensure_installed = {
                 'javascript',
                 'typescript',
+                'tsx',
                 'java',
                 'groovy',
                 'python',
