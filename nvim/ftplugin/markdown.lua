4f2b7949d227e76a061fa4ea17114a62142885e9
Mat Pataki <matpataki@gmail.com>
2025-03-06 22:32:37 -0500

    add a markdown-specific config for spell checking.

---
+++ nvim/ftplugin/markdown.lua
@@ -0,0 +1,16 @@
+vim.opt_local.spell = true
+vim.opt_local.spelllang = 'en_ca,en_gb'
+
+-- Simple spell suggestion popup
+vim.keymap.set('n', '<leader>zs', function()
+  local word = vim.fn.expand('<cword>')
+  local suggestions = vim.fn.spellsuggest(word, 10)
+  
+  if #suggestions == 0 then return end
+  
+  vim.ui.select(suggestions, {
+    prompt = "Spell suggestions for: " .. word
+  }, function(choice)
+    if choice then vim.cmd("normal ciw" .. choice) end
+  end)
+end, { buffer = true, desc = "Spell Suggestions" })
