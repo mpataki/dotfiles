b69a81f8601cb40cfd23c87361f801a4eb3688b5
Mat Pataki <matpataki@gmail.com>
2025-06-15 10:40:14 -0400

    markdown config

--- nvim/lua/mpataki/plugins/render-markdown.lua
+++ nvim/lua/mpataki/plugins/render-markdown.lua
@@ -1,23 +1,26 @@
 return {
     {
         'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        dependencies = { 
+            'nvim-treesitter/nvim-treesitter',
+            'nvim-tree/nvim-web-devicons'
+        },
         ---@module 'render-markdown'
         ---@type render.md.UserConfig
         opts = {
             file_types = { "markdown", "Avante" },
-            bullet = {
-                enabled = true,
-                icons = { '●', '○', '◆', '◇' },
-                ordered_icons = function(level, index, value)
-                    value = vim.trim(value)
-                    local value_index = tonumber(value:sub(1, #value - 1))
-                    return string.format('%d.', value_index > 1 and value_index or index)
-                end,
-                left_pad = 0,
-                right_pad = 1,
-                highlight = 'RenderMarkdownBullet',
-            },
+            -- bullet = {
+            --     enabled = true,
+            --     icons = { '●', '○', '◆', '◇' },
+            --     ordered_icons = function(level, index, value)
+            --         value = vim.trim(value)
+            --         local value_index = tonumber(value:sub(1, #value - 1))
+            --         return string.format('%d.', value_index > 1 and value_index or index)
+            --     end,
+            --     left_pad = 0,
+            --     right_pad = 1,
+            --     highlight = 'RenderMarkdownBullet',
+            -- },
             checkbox = {
                 enabled = true,
                 position = 'inline',
