e0b2576d33e706e963747a00dbc10b8faa076a4c
Mat Pataki <matpataki@gmail.com>
2026-02-19 16:24:21 -0500

    gnupg and direnv

--- misc_tools/setup_misc_tools.sh
+++ misc_tools/setup_misc_tools.sh
@@ -49,6 +49,8 @@ function setup_misc_tools() {
   install_package zoxide
   install_package fzf
   install_package bufbuild/buf/buf
+  install_package gnupg
+  install_package direnv
 
   if [[ -z `which atuin` ]]; then
     curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
