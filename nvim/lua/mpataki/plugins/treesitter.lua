c44801465e22e6790d90ec0eda30f0706583d205
Mat Pataki <matpataki@gmail.com>
2026-02-26 21:11:40 -0500

    fix treesetter config

--- nvim/lua/mpataki/plugins/treesitter.lua
+++ nvim/lua/mpataki/plugins/treesitter.lua
@@ -1,8 +1,10 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
+    lazy = false,
+    build = ':TSUpdate',
     config = function()
-        require('nvim-treesitter.config').setup {
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
+        require('nvim-treesitter.configs').setup {
             ensure_installed = {
                 'javascript',
                 'typescript',
@@ -24,46 +26,19 @@ return {
                 'xml',
                 'cpp',
                 'markdown',
-                "markdown_inline",
+                'markdown_inline',
                 'sql',
                 'ruby',
             },
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
             auto_install = false,
-
-            -- List of parsers to ignore installing (or "all")
-            -- ignore_install = { "javascript" },
 
             highlight = {
                 enable = true,
+                additional_vim_regex_highlighting = false,
+            },
 
-                -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-                -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-                -- the name of the parser)
-                -- list of language that will be disabled
-                -- disable = { "c", "rust" },
-                -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-                -- disable = function(lang, buf)
-                    --   local max_filesize = 100 * 1024 -- 100 KB
-                    -- local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    -- if ok and stats and stats.size > max_filesize then
-                    -- return true
-                    -- end
-                    -- end,
-
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = false,
-                },
-
-            -- Enable incremental selection based on tree-sitter
             incremental_selection = {
                 enable = true,
                 keymaps = {
@@ -74,11 +49,10 @@ return {
                 },
             },
 
-            -- Enable indentation based on tree-sitter
             indent = {
                 enable = true,
-                disable = { "python" }, -- Python indentation can be problematic
+                disable = { "python" },
             },
-            }
-        end
-    }
+        }
+    end
+}
